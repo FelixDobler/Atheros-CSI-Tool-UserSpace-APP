c965bf070fee33455dca8de01756f37d66fdcfbb
Felix Dobler <felix.dobler@uni-konstanz.de>
2025-04-09 16:53:43 +0200

    modify hostapd startup

--- hostapd-2.5/hostapd/start_hostapd.sh
+++ hostapd-2.5/hostapd/start_hostapd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#sudo restart networking
-sudo nmcli nm wifi off
-sudo rfkill unblock wlan
 
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 10.10.0.1/24 up
+sudo ifconfig wlan1 down
+sudo ifconfig wlan1 10.10.0.1/24 up
+
 sleep 1
 
 sudo service isc-dhcp-server restart
 sudo ./hostapd ./hostapd.conf
+
+# disable interface after hostapd terminated
+sudo ifconfig wlan1 down
